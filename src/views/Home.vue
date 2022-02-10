1f7cad2fb9a76146df8cb614c01316f456ee54cc
Ed Chong <5728235+ewchong@users.noreply.github.com>
2022-02-10 20:48:20 +0000

    test 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,15 @@
         <home-hero></home-hero>
         <campaign-cards></campaign-cards>
         testtt
-        <div>
-            <stripe-element-card />
+            <stripe-checkout
+      ref="checkoutRef"
+      mode="payment"
+      :pk="publishableKey"
+      :line-items="lineItems"
+      :success-url="successURL"
+      :cancel-url="cancelURL"
+      @loading="v => loading = v"
+    />
         </div>
     </div>
 </template>
@@ -12,14 +19,33 @@
 <script>
 import HomeHero from '@/components/HomeHero.vue';
 import CampaignCards from '@/components/CampaignCards';
-import { StripeElementCard } from '@vue-stripe/vue-stripe';
+import { StripeCheckout } from '@vue-stripe/vue-stripe';
 
 export default {
     name: 'Home',
     components: {
         HomeHero,
         CampaignCards,
-        StripeElementCard
+        StripeCheckout
     }
+,
+  data () {
+    this.publishableKey = "";
+    return {
+      loading: false,
+      lineItems: [
+        {
+          price: 'some-price-id', // The id of the one-time price you created in your Stripe dashboard
+          quantity: 1,
+        },
+      ],
+      successURL: 'your-success-url',
+      cancelURL: 'your-cancel-url',
+    };
+  },
+  // You will be redirected to Stripe's secure checkout page
+  submit () {
+    this.$refs.checkoutRef.redirectToCheckout();
+  },
 };
 </script>
