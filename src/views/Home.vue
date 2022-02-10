2f1f15f15b2ac06dbec77890b4bbe366132167cf
Ed Chong <5728235+ewchong@users.noreply.github.com>
2022-02-10 20:02:52 +0000

    test stripe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,24 @@
     <div class="home">
         <home-hero></home-hero>
         <campaign-cards></campaign-cards>
+        testtt
+        <div>
+            <stripe-element-card />
+        </div>
     </div>
 </template>
 
 <script>
 import HomeHero from '@/components/HomeHero.vue';
 import CampaignCards from '@/components/CampaignCards';
+import { StripeElementCard } from '@vue-stripe/vue-stripe';
 
 export default {
     name: 'Home',
     components: {
         HomeHero,
-        CampaignCards
+        CampaignCards,
+        StripeElementCard
     }
 };
 </script>
